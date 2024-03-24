076afc33c8eace3e1d75e098a19dfeaf4376291b
Ilyès <ilyesboubou.ib@gmail.com>
2024-03-25 00:16:17 +0100

    ajout routes

--- SAE401_V2/src/app/app.routes.ts
+++ SAE401_V2/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { BoxComponent } from './components/box/box.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { PromoComponent } from './components/promo/promo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: BoxComponent },
+    { path: 'specialite', component: FooterComponent },
+    { path: 'etudiant', component:  HeaderComponent },
+    { path: 'etudianthorsparis', component:  IntroComponent },
+    { path: 'etudiantparis', component: PanierComponent },
+    { path: 'formetudiant', component:  PromoComponent },
+
+  
+];
