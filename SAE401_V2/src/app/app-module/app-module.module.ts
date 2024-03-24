d4c30d5ab21ba808317337f1d9c7f07bd368a7ac
Ilyès <ilyesboubou.ib@gmail.com>
2024-03-25 00:37:40 +0100

    Update app-module.module.ts

--- SAE401_V2/src/app/app-module/app-module.module.ts
+++ SAE401_V2/src/app/app-module/app-module.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from '.  /header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModuleModule { }
+export class AppModule { }
